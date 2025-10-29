3e309465b5e6ce6977530e687508db8cb903dda7
Khergianii <giorgikhergiani23@gmail.com>
2025-10-29 20:29:11 +0400

    'commit'

---
+++ level08/classwork/cw1.py
@@ -0,0 +1,12 @@
+number = int (input("ჩაწერე ნომერი : "))
+
+if number>=90:
+    print("მაღალი ქულა მიიღე")
+elif number>=50:
+    print("საშუალო ქულა მიიღე")
+elif number>=30:
+    print("დაბალი ქულა მიიღე")
+elif number>=10:
+    print("ჩაიჭერი")
+else:
+    print("მეორედ აქ აღარ მოხვიდე")
